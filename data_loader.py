37a3120e79e3025922016d6682552fb5d55f8628
sam <hgl9@foxmail.com>
2025-05-20 04:16:09 +0800

    feature: support finance data training.

---
+++ data_loader.py
@@ -0,0 +1,208 @@
+import os
+import re
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler  # type: ignore
+from torch.utils.data import Dataset
+from utils.timefeatures import time_features
+
+'''
+    columns:
+    re_cols = [
+        'low',
+        'high',
+        'open',
+        'close',
+        'volume',
+        'rise',
+        'amplitude',
+        'turnover',
+        'hld_rate',
+    ]
+    data = data.reindex(columns=re_cols)
+'''
+
+COLUMNS = {
+    '时间': 'time',
+    '开盘': 'open',
+    '最高': 'high',
+    '最低': 'low',
+    '收盘': 'close',
+    '涨幅': 'rise',
+    '振幅': 'amplitude',
+    '总手': 'volume',
+    '金额': 'turnover',
+    '换手': 'hld_rate',
+}
+
+DTYPES_ST = {
+    '时间': str,
+    '开盘': float,
+    '最高': float,
+    '最低': float,
+    '收盘': float,
+    '涨幅': float,
+    '振幅': float,
+    '总手': int,
+    '金额': float,
+    '换手': float,
+}
+DTYPES_FT = {
+    'code': str,
+    #'date': str,
+    'open': float,
+    'high': float,
+    'low': float,
+    'close': float,
+    'balance': float,
+    'up_dn_1': float,
+    'up_dn_2': float,
+    'volume': int,
+    'turnover': float,
+    'position': int,
+    'year': int,
+    'month': int,
+    'day': int,
+    'week': int,
+    'wday': int,
+    'yday': int,
+    'mdays': int,
+    'weekend': int,
+    'month_b': int,
+    'month_e': int,
+    'quarter_b': int,
+    'quarter_e': int,
+    'year_b': int,
+    'year_e': int,
+    'leap_year': int,
+    'prev': int,
+    'next': int,
+}
+
+
+class STDataSet(Dataset):
+    def __init__(
+        self,
+        sizes: tuple[int, int, int],  # (seq_len, label_len, pred_len)
+        data_path: str = '~/.data/stock/debug/002594_day.csv',
+    ) -> None:
+        super().__init__()
+        self.scaler: StandardScaler = StandardScaler()
+        self._data_path: str = os.path.expanduser(data_path)
+        self._seq_len, self._label_len, self._pred_len = sizes
+        self._read_data()
+
+    def _read_data(self) -> None:
+        df = pd.read_csv(
+            self._data_path,
+            parse_dates=['date'],
+            date_format='%Y/%m/%d',
+            dtype=DTYPES_ST,
+        )
+        # 去掉第一条数据，原因是：第一条数据涨幅和振幅为空
+        df = df[1:]
+        df.rename(columns=COLUMNS, inplace=True)
+        df = df[df['turnover'] != '--']
+        df['turnover'] = df['turnover'].astype(np.int64)
+        # old code:
+        # df['rise_amp'] = df['rise_amp'].apply(lambda x: float(x[: -1 if x.endswith('%') else len(x)]))
+        # df['amplitude'] = df['amplitude'].apply(lambda x: float(x[2:-1]))
+        # df['hld_rate'] = df['hld_rate'].apply(lambda x: float(x[:-1]))
+        # stamp = pd.DataFrame({'date': df.time})
+        # data = df.drop(columns=['time'])
+        stamp = df[['date']]
+        data = df.drop(columns=['date'])
+        '''
+        rcols = [
+            'low',
+            'high',
+            'open',
+            'close',
+            'volume',
+            'rise',
+            'amplitude',
+            'turnover',
+            'hld_rate',
+        ]
+        data = data.reindex(columns=rcols)
+        '''
+        self.columns = list(data.columns)
+        self.scaler.fit(data.values)
+        stamp_features = time_features(stamp, 1, 'b')
+        if stamp_features is None:
+            raise ValueError("time_features returned None, cannot assign to self._stamp")
+        self._stamp = stamp_features
+        self._data = self.scaler.transform(data.values)
+        self._data_frame = df
+
+    def __getitem__(self, idx: int) -> tuple:
+        xa = idx
+        xb = xa + self._seq_len
+        ya = xb - self._label_len
+        yb = ya + self._label_len + self._pred_len
+
+        x = self._data[xa:xb]
+        y = self._data[ya:yb]
+        x_mark = self._stamp[xa:xb]
+        y_mark = self._stamp[ya:yb]
+
+        return x, y, x_mark, y_mark
+
+    def __len__(self) -> int:
+        return len(self._data) - (self._seq_len + self._pred_len)
+
+
+class FTDataSet(Dataset):
+
+    def __init__(
+        self,
+        sizes: tuple[int, int, int],
+        data_path: str = os.path.join(os.path.dirname(__file__), '.exchange/csv/utf8/c_2006_ft.csv'),
+    ) -> None:
+        super().__init__()
+        self.scaler: StandardScaler = StandardScaler()
+        self._data_path: str = os.path.expanduser(data_path)
+        self._seq_len, self._label_len, self._pred_len = sizes
+        self._read_data()
+
+    def _read_data(self) -> None:
+        df = pd.read_csv(self._data_path, parse_dates=['date'], date_format='%Y%m%d', dtype=DTYPES_FT)
+        # 去掉第一条数据，原因是：第一条数据涨幅和振幅为空
+        df = df[1:]
+        df['code'] = df['code'].apply(lambda x: re.sub(r'^[a-z]+', '', x, flags=re.IGNORECASE))
+        df['code1'] = df['code'].apply(lambda x: int(x[:2]))
+        df['code2'] = df['code'].apply(lambda x: int(x[2:]))
+        df.drop(columns=['code'], inplace=True)
+        # old code:
+        # df['rise_amp'] = df['rise_amp'].apply(lambda x: float(x[: -1 if x.endswith('%') else len(x)]))
+        # df['amplitude'] = df['amplitude'].apply(lambda x: float(x[2:-1]))
+        # df['hld_rate'] = df['hld_rate'].apply(lambda x: float(x[:-1]))
+        # stamp = pd.DataFrame({'date': df.time})
+        # data = df.drop(columns=['time'])
+        stamp = df[['date']]
+        data = df.drop(columns=['date'])
+        self.columns = list(data.columns)
+        self.scaler.fit(data.values)
+        stamp_features = time_features(stamp, 1, 'b')
+        if stamp_features is None:
+            raise ValueError("time_features returned None, cannot assign to self._stamp")
+        self._stamp = stamp_features
+        self._data = self.scaler.transform(data.values)
+        self._data_frame = df
+
+    def __getitem__(self, idx: int) -> tuple:
+        xa = idx
+        xb = xa + self._seq_len
+        ya = xb - self._label_len
+        yb = ya + self._label_len + self._pred_len
+
+        x = self._data[xa:xb]
+        y = self._data[ya:yb]
+        x_mark = self._stamp[xa:xb]
+        y_mark = self._stamp[ya:yb]
+
+        return x, y, x_mark, y_mark
+
+    def __len__(self) -> int:
+        return len(self._data) - (self._seq_len + self._pred_len)
---
+++ main_user.py
@@ -0,0 +1,199 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+import copy
+import torch
+import numpy as np
+import statistics as stat
+from torch import nn
+from torch import optim
+from torch.utils.data import DataLoader
+from models.model import Informer
+from data_loader import STDataSet
+from data_loader import FTDataSet
+
+
+class MyLR:
+    def __init__(self, optimizer: optim.Adam, step_rate: float = 0.9, threshold: int = 1) -> None:
+        self._optimizer = optimizer
+        self._last_lr = [group['lr'] for group in self._optimizer.param_groups]
+        self._best_score = float('inf')
+        self._last_score = float('inf')
+        self._best_steps = 0
+        self._step_rate = step_rate
+        self._threshold = threshold
+
+    def step(self, score: float) -> None:
+        '''
+        if score < self._best_score:
+            self._best_score = score
+            self._best_steps = 0
+            return
+        self._best_steps += 1
+        if self._best_steps < self._threshold:
+            return
+        self._best_steps = 0
+        '''
+        last_score = self._last_score
+        self._last_score = score
+        if score <= last_score:
+            return
+        self._last_lr = [lr * self._step_rate for lr in self._last_lr]
+        for i, group in enumerate(self._optimizer.param_groups):
+            group['lr'] = self._last_lr[i]
+        print(f'Update lr to: {self._last_lr}')
+
+
+class EarlyStop(object):
+    def __init__(self, patience: int = 15) -> None:
+        self._patience: int = patience or 15
+        self._counter: int = 0
+        self._best_sore: float = float('inf')
+        self._train_loss: float = float('inf')
+        self._valid_loss: float = float('inf')
+
+    def stop(self, valid_loss: float) -> bool:
+        if valid_loss < self._best_sore:
+            self._best_sore = valid_loss
+            self._counter = 0
+            return False
+
+        self._counter += 1
+        return self._counter > self._patience
+
+
+DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+BATCH_SIZE = 64
+EPOCHS = int(1e9)
+
+seq_len = 20
+label_len = 20
+pred_len = 5
+
+dataset = FTDataSet((seq_len, label_len, pred_len))
+
+enc_in = dataset[0][0].shape[-1]
+dec_in = enc_in
+c_out = 2  # enc_in  # 4
+
+# e_layers > 5 and d_layers > 4 will encountered error
+e_layers = 8
+d_layers = 8
+
+model = Informer(
+    enc_in,
+    dec_in,
+    c_out,
+    seq_len,
+    label_len,
+    pred_len,  # out_len,
+    factor=5,
+    d_model=512,
+    n_heads=8,
+    e_layers=e_layers,
+    d_layers=d_layers,
+    d_ff=512,
+    dropout=0.0,
+    attn='full',  # 'prob',
+    embed='timeF',  # 'fixed',
+    freq='b',
+    activation='gelu',
+    output_attention=False,
+    distil=True,
+    mix=True,
+    device=DEVICE,
+).to(DEVICE)
+
+model_lr = 1e-1
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=model_lr, weight_decay=0.0)
+lr_scheduler = MyLR(optimizer)
+
+
+def process_one_batch(
+    batch_x: torch.Tensor, batch_y: torch.Tensor, batch_x_mark: torch.Tensor, batch_y_mark: torch.Tensor
+) -> tuple[torch.Tensor, torch.Tensor]:
+    batch_x = batch_x.float().to(DEVICE)
+    batch_y = batch_y.float()
+
+    batch_x_mark = batch_x_mark.float().to(DEVICE)
+    batch_y_mark = batch_y_mark.float().to(DEVICE)
+
+    # decoder input
+    dec_inp = torch.zeros([batch_y.shape[0], pred_len, batch_y.shape[-1]]).float()
+    dec_inp = torch.cat([batch_y[:, :label_len, :], dec_inp], dim=1).float().to(DEVICE)
+    # encoder - decoder
+    outputs = model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+    # field:
+    # f_dim = -1  # predict multiple value
+    # batch_y = batch_y[:, -pred_len:, f_dim:].to(DEVICE)
+    batch_y = batch_y[:, -pred_len:, :c_out].to(DEVICE)
+
+    return outputs, batch_y
+
+
+def train() -> None:
+    train_loader = DataLoader(dataset, batch_size=BATCH_SIZE, drop_last=True, num_workers=0)
+    train_loss = [0.0] * len(train_loader)
+    best_model_dict = None
+    best_score = float('inf')
+    early_stop = EarlyStop(30)
+    for epoch in range(EPOCHS):
+        for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
+            optimizer.zero_grad()
+            pred, true = process_one_batch(batch_x, batch_y, batch_x_mark, batch_y_mark)
+            loss: torch.Tensor = criterion(pred, true)
+            train_loss[i] = loss.item()
+            loss.backward()
+            optimizer.step()
+
+        loss_avg = stat.mean(train_loss)
+        lr_scheduler.step(loss_avg)
+        if loss_avg < best_score:
+            best_model_dict = copy.deepcopy(model.state_dict())
+            best_score = loss_avg
+        print(f'{epoch:>2d}: loss: {loss_avg}, best: {best_score}, stop: {early_stop._counter}')
+        if early_stop.stop(loss_avg):
+            break
+
+    if best_model_dict is not None:
+        model.load_state_dict(best_model_dict)
+    else:
+        raise RuntimeError("No best model found to load.")
+
+    save_data = {
+        'model': best_model_dict,
+        'optimizer': optimizer.state_dict(),
+    }
+    model_path = './pth/informer.pth'
+    torch.save(save_data, model_path)
+    print(f'score: {best_score}, Save model to { model_path}!')
+
+
+def valid() -> tuple[np.ndarray, np.ndarray]:
+    model_path = './pth/informer.pth'
+    pth = torch.load(model_path, weights_only=True)
+    model.load_state_dict(pth['model'])
+    model.eval()
+    valid_loader = DataLoader(dataset, batch_size=BATCH_SIZE, drop_last=False, num_workers=0)
+    pred_list = []
+    true_list = []
+    with torch.no_grad():
+        for batch_x, batch_y, batch_x_mark, batch_y_mark in valid_loader:
+            pred, true = process_one_batch(batch_x, batch_y, batch_x_mark, batch_y_mark)
+            pred_list.append(pred.cpu().numpy())
+            true_list.append(true.cpu().numpy())
+
+    preds = np.concatenate(pred_list)
+    trues = np.concatenate(true_list)
+    ps = dataset.scaler.inverse_transform(preds.reshape(-1, dataset._data.shape[-1]))
+    ts = dataset.scaler.inverse_transform(trues.reshape(-1, dataset._data.shape[-1]))
+    preds = ps.reshape(-1, preds.shape[-2], preds.shape[-1])
+    trues = ts.reshape(-1, trues.shape[-2], trues.shape[-1])
+    return preds, trues
+
+
+if __name__ == '__main__':
+    # train()
+    preds, trues = valid()
+    print(preds.shape)
+    print(trues.shape)
