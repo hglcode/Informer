aa8779059d017778c8d32781d8f7e226e82b74a8
sam <hgl9@foxmail.com>
2025-05-24 16:08:05 +0800

    refactor: type annotation for masking.py.

--- utils/masking.py
+++ utils/masking.py
@@ -1,24 +1,24 @@
 import torch
 
-class TriangularCausalMask():
-    def __init__(self, B, L, device="cpu"):
-        mask_shape = [B, 1, L, L]
+
+class TriangularCausalMask:
+    def __init__(self, b: int, l: int, device: str = "cpu") -> None:
+        mask_shape = [b, 1, l, l]
         with torch.no_grad():
             self._mask = torch.triu(torch.ones(mask_shape, dtype=torch.bool), diagonal=1).to(device)
 
     @property
-    def mask(self):
+    def mask(self) -> torch.Tensor:
         return self._mask
 
-class ProbMask():
-    def __init__(self, B, H, L, index, scores, device="cpu"):
-        _mask = torch.ones(L, scores.shape[-1], dtype=torch.bool).to(device).triu(1)
-        _mask_ex = _mask[None, None, :].expand(B, H, L, scores.shape[-1])
-        indicator = _mask_ex[torch.arange(B)[:, None, None],
-                             torch.arange(H)[None, :, None],
-                             index, :].to(device)
+
+class ProbMask:
+    def __init__(self, b: int, h: int, l: int, index: torch.Tensor, scores: torch.Tensor, device: str = "cpu") -> None:
+        _mask = torch.ones(l, scores.shape[-1], dtype=torch.bool).to(device).triu(1)
+        _mask_ex = _mask[None, None, :].expand(b, h, l, scores.shape[-1])
+        indicator = _mask_ex[torch.arange(b)[:, None, None], torch.arange(h)[None, :, None], index, :].to(device)
         self._mask = indicator.view(scores.shape).to(device)
-    
+
     @property
-    def mask(self):
+    def mask(self) -> torch.Tensor:
         return self._mask
